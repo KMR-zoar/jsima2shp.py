7e95a10a73ad40a44639a6b52ae4c5bc6c3b1cc6
zoar <zoar@k-side.net>
2019-09-27 09:46:52 +0900

    エラー処理方法を追加

--- module/xmlReader.py
+++ module/xmlReader.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 
 def readXML(path):
   # SIMAーJPGIS は規格上の文字コードがShift-JISに指定されている
-  xmlStream = codecs.open(path, 'r', 'shift_jis')
+  xmlStream = codecs.open(path, 'r', 'shift_jis', 'ignore')
   xmlString = ''.join(xmlStream)
   xmlData = ET.fromstring(xmlString)
 
